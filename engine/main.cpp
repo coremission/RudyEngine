391afe11bac193e798f4d0a10f546685d083eb7f
Sergey Kormishin <skormishin@harborcrew.com>
2017-02-27 19:50:30 +0300

    main as in opengl-boilerplate

--- engine/main.cpp
+++ engine/main.cpp
@@ -20,8 +20,7 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
 }
 
 static void render(GLFWwindow *window) {
-	glClearColor(1.0f, 0, 0, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_ACCUM_BUFFER_BIT);
 }
 
 int main(void) {

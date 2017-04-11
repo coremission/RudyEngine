fd8a4f41661ebd0144f02c66f16df64507e1c3b3
Sergey Kormishin <skormishin@harborcrew.com>
2017-04-11 17:36:54 +0300

    breakout shaders path fix

--- games/breakout/main.cpp
+++ games/breakout/main.cpp
@@ -11,20 +11,9 @@
 
 using namespace std;
 
-void _do(int, char**);
 void setUpScene();
 
-int main(int argc, char **argv)
-{
-	_do(argc, argv);
-	cout << endl << "Press any key to exit..." << endl;
-	cin.get();
-
-	return 0;
-}
-
-// this one exist to see destructors outputs
-void _do(int argc, char **argv) {
+int main(int argc, char **argv) {
 	try {
 		Application::initialize(&argc, argv);
 		
@@ -38,6 +27,10 @@ void _do(int argc, char **argv) {
 		Application::exit();
 		cout << exc.what();
 	}
+	cout << endl << "Press any key to exit..." << endl;
+	cin.get();
+
+	return 0;
 }
 
 void setUpScene() {
@@ -47,14 +40,14 @@ void setUpScene() {
 	ball->AddComponent<BallBehaviour>(ballBehaviour);
 
 	// create renderer
-	ball->renderer = make_unique<SpriteRenderer>(ball, "Sprites\\ball.png");
+	ball->renderer = make_unique<SpriteRenderer>(ball, "sprites/ball.png");
 	
 	// PADDLE
 	GameObject* paddle = new GameObject("paddle");
 	PaddleBehaviour* paddleBehaviour = new PaddleBehaviour(paddle);
 	paddleBehaviour->setUpBall(ballBehaviour);
 	paddle->AddComponent(paddleBehaviour);
-	paddle->renderer = make_unique<SpriteRenderer>(paddle, "Sprites\\paddle.png");
+	paddle->renderer = make_unique<SpriteRenderer>(paddle, "sprites/paddle.png");
 	
 	// CONTROLLER
 	GameObject* controller = new GameObject("controller");
